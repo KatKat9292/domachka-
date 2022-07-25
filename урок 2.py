259d223a37e927db1517a1baeb1c69abfa6a31c9
KatKat9292 <Katty_melkaya@mail.ru>
2022-07-25 22:04:08 +0300

    home work

---
+++ урок 2.py
@@ -0,0 +1,15 @@
+""""""
+a = [input("enter a number")]
+b = [input("enter a number")]
+print(a, b,  sep=",")
+""""""
+
+""""""
+a = (input("enter a number"))
+b = (input("enter a number"))
+print(a, b,  sep=",")
+""""""
+
+
+
+
