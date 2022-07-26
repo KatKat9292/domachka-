7515caeab4ced6a9f75d60825e16ed65f3328c37
KatKat9292 <Katty_melkaya@mail.ru>
2022-07-26 20:55:59 +0300

    work

--- lesson 8.py
+++ lesson 8.py
@@ -1,8 +1,18 @@
-def vowel_counter(array: str) -> int:
+"""def vowel_counter(array: str) -> int:
     vowels = ['a', 'e', 'i', 'o', 'u', 'y']
     counter = 0
     for letter in list(array):
         if letter in vowels:
             counter += 1
             return print(counter)
-vowel_counter("heellow asd")
+vowel_counter("heellow asd")"""
+
+def remove_vowel(array: str) -> str:
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y']
+    for element in array:
+        if element in vowels:
+            array = array.replace(element, '')
+    return array
+print(remove_vowel("heellow asd"))
+
+
