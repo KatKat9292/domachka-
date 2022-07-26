9657adb3dd6ad59e1dd018fde4f398bb0303ca18
KatKat9292 <Katty_melkaya@mail.ru>
2022-07-26 22:00:20 +0300

    centre elemr=ent task

--- lesson 8.py
+++ lesson 8.py
@@ -7,12 +7,27 @@
             return print(counter)
 vowel_counter("heellow asd")"""
 
-def remove_vowel(array: str) -> str:
+""""def remove_vowel(array: str) -> str:
     vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y']
     for element in array:
         if element in vowels:
             array = array.replace(element, '')
     return array
-print(remove_vowel("heellow asd"))
+print(remove_vowel("heellow asd"))"""
 
+"""def digits_counter(nums: str) -> list:
+    new_nums = nums.split()
+    numbers = [int(i) for i in new_nums]
+    return [sorted(numbers)[0], sorted(numbers)[-1]]
+print(digits_counter("1 2 -5 3 12"))"""
+
+
+def centre(array: str) -> str:
+    ln = len(array)
+    if len(array) % 2 == 0:
+        array = array[int(len(array)/2 - 1 ): int(len(array)/2 + 1)]
+    else:
+        array = array[len(array) // 2]
+    return array
+print(centre("testt"))
 
